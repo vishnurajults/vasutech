d50e14e187f75bd0cdfe677f5df7436185275af3
Vishnu Raj A <vishnu.raj@ults.in>
2021-08-01 05:13:16 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import vasu_logo from './vasu_logo.PNG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={vasu_logo} alt="vasu_logo"/>
+        <h1>VASU Tech Era Begins !!!!!</h1>
+        {/* <h1>Hello World!</h1> */}
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
